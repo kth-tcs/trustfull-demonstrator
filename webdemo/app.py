626dc26361554f22491e06e88d8306e4a6629a4d
Orestis Floros <forestis@kth.se>
2020-04-04 20:29:24 +0200

    Init Flask app

---
+++ webdemo/app.py
@@ -0,0 +1,46 @@
+import os
+
+from flask import Flask, render_template, request, send_file
+
+app = Flask(__name__)
+
+POLL_DATA = {
+    "question": "Who do you vote for?",
+    "fields": ("Napoleon", "George Bush", "Christina, Queen of Sweden"),
+}
+FILENAME = "data.txt"
+
+
+@app.route("/")
+def root():
+    return render_template("poll.html", data=POLL_DATA)
+
+
+@app.route("/poll")
+def poll():
+    vote = request.args.get("field")
+
+    with open(FILENAME, "a") as f:
+        print(vote, file=f)
+
+    return render_template("thankyou.html", data=POLL_DATA)
+
+
+@app.route("/reset")
+def reset():
+    if os.path.exists(FILENAME):
+        stat = os.stat(FILENAME)
+        os.remove(FILENAME)
+        return f"Succesfully deleted {FILENAME}:<br/><pre>   {stat}</pre>"
+    return "Nothing to do!"
+
+
+@app.route("/results")
+def results():
+    if os.path.exists(FILENAME):
+        return send_file(FILENAME)
+    return "No results found", 404
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
